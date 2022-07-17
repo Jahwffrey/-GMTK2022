3212d6dbdb121129934b9d8dfd7945830092ecbe
Jt <johnjackthomason@gmail.com>
2022-07-17 09:01:18 -0700

    Audio clips and tweaking

--- Assets/Scripts/DiceUnit.cs
+++ Assets/Scripts/DiceUnit.cs
@@ -98,6 +98,8 @@ public class DiceUnit : MonoBehaviour
     protected bool DuringStep;
     protected bool StopNextOnGound;
 
+    protected AudioSource AudioSource;
+
     // Game steps work by units adding several actions to a stack
     // Once 'ExecuteNextAction' is called, the next item on the
     // stack is executed. When there are no actions left, EndStep
@@ -110,6 +112,11 @@ public class DiceUnit : MonoBehaviour
     protected Vector3 PositionWhenPassedFinishLine;
 
     protected bool Dead;
+
+    public AudioClip SpawnSound;
+    public AudioClip HurtSound;
+    public AudioClip DeathSound;
+    public AudioClip VictorySound;
 
     public string GetInfoText()
     {
@@ -149,6 +156,7 @@ public class DiceUnit : MonoBehaviour
         Rigidbody = GetComponent<Rigidbody>();
         HealthBar = GetComponentInChildren<HealthBar>();
         WhichPlayerIndicator = GetComponentInChildren<WhichPlayerIndicator>();
+        AudioSource = GetComponentInChildren<AudioSource>();
         Health = MaxHealth;
         InheritableAwake();
     }
@@ -156,6 +164,7 @@ public class DiceUnit : MonoBehaviour
     protected bool DiceSet = false;
     void Start()
     {
+        PlaySound(SpawnSound);
         if (!DiceSet)
         {
             Brain = UnitController.NothingDie();
@@ -248,6 +257,7 @@ public class DiceUnit : MonoBehaviour
         {
             if (CheckIfPassedFinishLine())
             {
+                PlaySound(VictorySound);
                 PassedFinishLine = true;
                 Controller.UnitPassedFinishLine(this);
                 PositionWhenPassedFinishLine = transform.position;
@@ -431,6 +441,11 @@ public class DiceUnit : MonoBehaviour
 
     public virtual void TakeDamage(float amt, Vector3 knockback)
     {
+        if(amt > 0)
+        {
+            PlaySound(HurtSound);
+        }
+
         Health -= amt;
         Rigidbody.velocity += knockback;
         DisplayHealth();
@@ -463,6 +478,7 @@ public class DiceUnit : MonoBehaviour
 
     public void Die()
     {
+        PlaySound(DeathSound);
         DisplayHealth();
         RemoveFromConsideration();
         Dead = true;
@@ -538,5 +554,14 @@ public class DiceUnit : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         InheritableOnTriggerStay(other);
+    }
+
+    protected void PlaySound(AudioClip clip)
+    {
+        if(clip != null && AudioSource != null)
+        {
+            AudioSource.clip = clip;
+            AudioSource.Play();
+        }
     }
 }
